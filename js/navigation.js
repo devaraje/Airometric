62e611a63de189994ee19bd5159b06ab5fc98fb8
Evelyn Devaraj <devarajevelyn@gmail.com>
2016-06-08 23:23:41 -0700

    use local storage

--- js/navigation.js
+++ js/navigation.js
@@ -1,30 +1,23 @@
-//event handler for when the window loads
+//event handler for window load
 //do not show the logo in the header if the window size is < 800px
-
 $(window).load(function() {
   
   if ($(window).width() <= 800){
         
         $('.nav-font').css('font-size', '1.5em');
         $('#nav-logo').hide();
-		/*$('#nav-logo-small').show();*/
-        /*$('#carousel-cap').hide();*/
-        /*$('#carousel-cap-small').show();*/
-        /*$('.carousel-indicators').hide();
        
   }
   else {
     
     $('#nav-logo-small').hide();
-   /* $('#nav-logo').show();
-    $('#carousel-cap').show();*/
-    /*$('#carousel-cap-small').hide();*/
+
   }
   
 });
 
 
-
+//event handler for window scroll
 /*$( window ).scroll(function() {
   
   if ($(window).width() <= 800){
--- js/popover.js
+++ js/popover.js
@@ -1,17 +1,21 @@
 
-//Click handler for the sign up today button
+//Click handler for the "Sign Up Today" button
+//Displays a modal dialog
 $("#signuptoday").click(function (){
+  
     $("#signupModal").modal ();
+  
 });
 
 
 $('#submitdetails').off( );
 
-//click handler for the submit details button
-//sends a POST request to the server
+//Click handler for the "Submit Details button
+//Stores the name of the user in local storage
+//Sends a POST request to the server with all the details entered by the user in the form
 $('#submitdetails').on( 'click', function( evt ) {
   
-    alert("In submitdetails click handler!");
+    //alert("In submitdetails click handler!");
   
     var baseUrl = ' https://shielded-sea-3725.herokuapp.com/data-api/';
     var collection = 'edeveraj'; 
@@ -19,10 +23,18 @@ $('#submitdetails').on( 'click', function( evt ) {
     var emailid = $('#emailid').val();
     var phonenum = $('#phonenum').val();
 
-    alert(pname);
     evt.preventDefault( );
+  
+    //write "pname" to local storage
+    writeToLS(pname);
+  
+    //update header with customized message containing user's name
+    updateHeader();
 
-    $.ajax( baseUrl + collection,
+  //Getting an error when I make the POST request below: 
+  //XMLHttpRequest cannot load https://shielded-sea-3725.herokuapp.com/data-api/edeveraj. Response to preflight request doesn't pass access   //control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://127.0.0.1:53809' is           //therefore not allowed access. 
+  
+  /*  $.ajax( baseUrl + collection,
     {
         method: 'POST',
         data: {
@@ -32,8 +44,52 @@ $('#submitdetails').on( 'click', function( evt ) {
               },
         success: logCreateResult,
         error: logAjaxError
-    } );
+    } );*/
+  
 } );
+
+//update header when modal dismissed
+$('#signUpModal').on('hidden', function(){
+  
+  updateHeader();
+  
+});
+
+//update header on window load
+$(window).load(function(){
+  
+  updateHeader();
+  
+});
+
+//Write to local storage
+function writeToLS(username) {
+  
+  //alert("in writeToLS");
+  localStorage[ "username" ] = JSON.stringify( username );
+}
+
+//Read from local storage
+function readFromLS() {
+  
+  //alert("in readFromLS");
+  return JSON.parse( localStorage[ "username" ] );
+  
+}
+
+//Read user name from local storage and update header  
+function updateHeader() {
+  
+  //alert("in updateHeader");
+  var username = readFromLS();
+  
+  if (username !== null){
+    var sp = "<span>Welcome " + username + "!</span>"
+    $( '#welcome' ).empty();
+    $( sp ).appendTo( "#welcome" );
+  }
+}
+
 
 function logCreateResult( data ) {
     console.log( 'Data received:', data );
